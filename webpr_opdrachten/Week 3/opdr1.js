fe6f5725044536817cdbf63f46075705b39ffee4
Dylan Toirkens <dylan.toirkens@outlook.com>
2017-03-13 18:55:04 +0100

    Add: Denkvragen week 3 Add: First exercise week 3

---
+++ webpr_opdrachten/Week 3/opdr1.js
@@ -0,0 +1,8 @@
+﻿var array = ["Een", "Twee", "Drie", "Vier", "Vijf", "Zes", "Zeven", "Acht", "Negen", "Tien"];
+array.sort(function (a, b) {return a.length - b.length});
+
+var targetNode = document.getElementById("inhoud");
+var textNode = document.createTextNode(array);
+
+targetNode.appendChild(textNode);
+
